41ccfc197f470765c3d82ec852a052390ee91062
Curtis Rutland <curtisrutland@gmail.com>
2025-08-03 09:59:13 -0500

    updated protocol to work on windows

--- src/protocols/index.tsx
+++ src/protocols/index.tsx
@@ -3,12 +3,24 @@ import { net, protocol } from "electron";
 export const imageBrowserProtocolName = "image-browser";
 
 export function registerPriviledgedSchemes() {
-  protocol.registerSchemesAsPrivileged([{ scheme: imageBrowserProtocolName, privileges: { bypassCSP: true } }]);
+  protocol.registerSchemesAsPrivileged([{ scheme: imageBrowserProtocolName, privileges: { bypassCSP: true, secure: true, supportFetchAPI: true,standard: true } }]);
 }
 
 export function registerProtocolHandlers() {
   protocol.handle(imageBrowserProtocolName, async (request) => {
+    if(process.platform === "win32") {
+      return handleWindowsFileProtocol(request);
+    } 
     const url = request.url.replace(`${imageBrowserProtocolName}://`, "file://");
     return net.fetch(url);
   });
 }
+
+function handleWindowsFileProtocol(request: Request) {
+  const base = request.url.replace(`${imageBrowserProtocolName}://`, "");
+  const [drive, ...rest] = base.split("/");
+  const newBase = `${drive}:/${rest.join("/")}`;
+  const url = `file://${newBase}`;
+  return net.fetch(url);
+}
+
